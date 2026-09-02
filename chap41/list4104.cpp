66b7ac6392105ae64b6f1379f8293f7d37ea2902
LiXiaqin <lixiaqin0301@gmail.com>
2026-09-03 07:20:29 +0800

    chap41

--- chap41/list4104.cpp
+++ chap41/list4104.cpp
@@ -18,13 +18,18 @@ main(int argc, char *argv[])
         }
     }
 
-    std::vector<std::string> data { std::istream_iterator<std::string>(std::cin), std::istream_iterator<std::string>() };
+    std::vector<std::string> data {
+        std::istream_iterator<std::string>(std::cin),
+        std::istream_iterator<std::string>()
+    };
 
     // Ensure at least one string to measure.
     if (data.empty()) {
         data.emplace_back();
     }
-    auto longest { std::ranges::max(data, [](std::string const &a, std::string const &b) { return a.size() < b.size(); }) };
+    auto longest { std::ranges::max(data, [](std::string const &a, std::string const &b) {
+        return a.size() < b.size();
+    }) };
     std::cout << "Longest string is \"" << longest << "\"\n";
 
     return 0;
