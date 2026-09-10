3a64eb25bf4b7f82b3d221ce3bb878211ce9937c
LiXiaqin <lixiaqin0301@gmail.com>
2026-09-11 07:28:14 +0800

    chap45

--- chap45/list4501.cpp
+++ chap45/list4501.cpp
@@ -2,11 +2,20 @@
 /** Listing 45-1. Searching for an Integer */
 #include "data.hpp"
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 
 int
-main()
+main(int argc, char *argv[])
 {
+    std::ifstream input;
+    if (argc > 0 && argv[0] != nullptr) {
+        input.open(std::string(argv[0]) + ".input");
+        if (input.is_open()) {
+            std::cin.rdbuf(input.rdbuf());
+        }
+    }
+
     intvector data {};
     read_data(data);
     write_data(data);
